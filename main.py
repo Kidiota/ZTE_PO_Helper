33a571b12ca8e4a5a3798c17fbf940e58b59b3c8
Kidiota <17718520426@163.com>
2025-05-16 15:40:35 +0800

    master

---
+++ main.py
@@ -0,0 +1,60 @@
+import pdfplumber
+import PyPDF2
+import os
+
+#修复CropBox问题
+def fix_cropbox(pdf_path, output_path):
+    """修复PDF文件的CropBox问题，并生成新的PDF文件."""
+    with open(pdf_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        writer = PyPDF2.PdfWriter()
+
+        for page in reader.pages:
+            if "/CropBox" not in page:
+                page.cropbox = page.mediabox
+            writer.add_page(page)
+
+    with open(output_path, "wb") as output_file:
+        writer.write(output_file)
+
+
+
+#读取input文件夹内文件的文件名
+folder_path = "input"
+filesName = os.listdir(folder_path)             #以列表方式记录所有文件名
+
+#循环输出PO号和金额
+i = 0
+while i < len(filesName):
+    fileFullName = "input\\" + filesName[i]
+    #提取pdf文字
+    with pdfplumber.open(fileFullName) as pdf:
+        firstPage = pdf.pages[0]                    #指定页码
+        firstPageText = firstPage.extract_text()    #提取文本
+
+        #分离PO_Number
+        poNum = firstPageText[firstPageText.find('PO Number : ') + 12 : firstPageText.find('PO Number : ') + 22]       #默认PO Number 为十位数字
+        #print(poNum)
+
+        #分离 TWENTY-TWO AND SEVENTY-SIX CENT ONLY 
+        endPage = pdf.pages[-1]                     #指定页码
+        endPageText = endPage.extract_text()        #提取文本
+
+        #计算总价开始位置
+        totalAmountStart = endPageText.find(' CENT ONLY ') + 11
+
+        #计算总价结束位置
+        totalAmountEnd = endPageText.find('Should the Sales Tax/Service Tax value stated in the Purchase Order differs from the Supplier')
+
+
+        #分离总价
+        totalAmount = endPageText[totalAmountStart : totalAmountEnd]
+        
+    #显示结果
+    print(poNum, "    ", totalAmount, "    ", i)
+    i = i + 1
+
+
+
+
+    
