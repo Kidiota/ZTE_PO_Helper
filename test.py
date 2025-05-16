75a54f7c12d8f35e2821bebae97efab0b5b1d7e0
Kidiota <17718520426@163.com>
2025-05-16 12:17:12 +0800

    master

---
+++ test.py
@@ -0,0 +1,11 @@
+# 提取pdf文字
+import pdfplumber
+#with pdfplumber.open("c:\\Users\\DuangRui\\Documents\\GitHubProjects\\ZTE_PO_Helper\\pdfs\\01.pdf") as pdf:
+with pdfplumber.open("pdfs\\16.pdf") as pdf:
+    page01 = pdf.pages[0] #指定页码
+    text = page01.extract_text()#提取文本
+    #print(text)
+    #print("data type: ", type(text))
+    print(text.find('PO Number : '))
+    print(text[42:52])
+
